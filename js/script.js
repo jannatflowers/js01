a6f5608a826d11b2b991cd332781812bd5a0f8ae
jannatflowers <68329669+jannatflowers@users.noreply.github.com>
2020-10-23 21:03:40 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function jan(){
+  document.getElementById("ja").innerHTML ="sunrise";
+}
+function btn1(){
+  document.getElementById("btnhead").style.color="red";
+}
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+function btn3(){
+  document.getElementById("fbicon").style.display="none"
+}
+function tab11(){
+  document.getElementById("tab1").style.color="red";
+}
+function tab22(){
+  document.getElementById("tab223").style.color="pink";
+}
+function tab33(){
+  document.getElementById("tab333").style.color="blue";
+}
+function clickbtn(){
+  document.getElementById("clikme").style.display="block";
+}
+
+
+	var x,t,x;
+x=20;
+t= 20;
+x=20+20;
+document.getElementById('variable').innerHTML=x;
+
+var ru= "rupa";
+var ja=20;
+var na= 130;
+document.getElementById('variable2').innerHTML=ru+ja+na;
+
+var  jannat=20+30+"nahar";
+document.getElementById('variable3').innerHTML=jannat;
+var rupa='26',nahar="25",jannat=22;
+document.getElementById('variable4').innerHTML=rupa;
+
+function hello(){
+document.getElementById('variable').innerHTML="hi";
+document.getElementById('variable').innerHTML="thi is jannatflowrs  startment";
+}
